00b270797bc8a96fc7c43972f93b547451cef1b0
tyja358-rgb <tyja358@gmail.com>
2026-01-07 04:07:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,24 @@
 
+const TelegramBot = require('node-telegram-bot-api');
+
+// Вставь сюда токен твоего бота
+const token = '8465624288:AAGKPaKjgGKxOZ3e1xN4pFTI_gTzP3KiVjc';
+const bot = new TelegramBot(token, { polling: true });
+
+// Команда /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  
+  bot.sendMessage(chatId, 'Добро пожаловать! Нажмите кнопку, чтобы открыть Plasma Tower:', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Открыть Plasma Tower',
+            url: 'https://tyja358-rgb.github.io/miniapp/'
+          }
+        ]
+      ]
+    }
+  });
+});
