23a20a30133e87fccee60cd9d65ca907ad4cc222
tyja358-rgb <tyja358@gmail.com>
2026-01-07 03:51:45 +0200

    Update bot.js

--- public/bot.js
+++ public/bot.js
@@ -1,33 +1,23 @@
-// bot.js
-// Пример кнопки для Telegram Bot, которая открывает миниап прямо в браузере
-
 const TelegramBot = require('node-telegram-bot-api');
 
-// Вставь сюда свой токен бота
+// Вставь сюда токен твоего бота
 const token = '8465624288:AAGKPaKjgGKxOZ3e1xN4pFTI_gTzP3KiVjc';
 const bot = new TelegramBot(token, { polling: true });
 
-// Обработка команды /start
+// Команда /start
 bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-
-    const opts = {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: 'Открыть Плазма Товер',
-                        url: 'https://tyja358-rgb.github.io/miniapp/' // <-- ссылка на GitHub Pages
-                    }
-                ]
-            ]
-        }
-    };
-
-    bot.sendMessage(chatId, 'Добро пожаловать в Plasma Tower! Нажмите кнопку ниже, чтобы открыть игру:', opts);
-});
-
-// Опционально: обработка других команд
-bot.onText(/\/help/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'Используйте кнопку "Открыть Плазма Товер" чтобы запустить игру.');
+  const chatId = msg.chat.id;
+  
+  bot.sendMessage(chatId, 'Добро пожаловать! Нажмите кнопку, чтобы открыть Plasma Tower:', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Открыть Plasma Tower',
+            url: 'https://tyja358-rgb.github.io/miniapp/'
+          }
+        ]
+      ]
+    }
+  });
 });
