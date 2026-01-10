a14813747cd20a2b39c28e537c3e7f260026e0e4
tyja358-rgb <tyja358@gmail.com>
2026-01-10 07:01:25 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+from telegram.ext import Updater, CommandHandler
+import os
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+def start(update, context):
+    update.message.reply_text("Бот запущен ✅")
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+
+updater.start_polling()
+updater.idle()
